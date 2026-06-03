a8f499a9c618fb7f2d9dc79e03c3442381d67d17
Rosana6574 <rosa.inportant20@gmail.com>
2026-06-03 09:43:12 -0300

    Add files via upload

---
+++ lib/pagesfront/formatters.dart
@@ -0,0 +1,55 @@
+import 'package:flutter/services.dart';
+
+class CpfInputFormatter extends TextInputFormatter {
+  @override
+  TextEditingValue formatEditUpdate(
+    TextEditingValue oldValue,
+    TextEditingValue newValue,
+  ) {
+    final text = newValue.text;
+    if (text.isEmpty) return newValue;
+
+    final buffer = StringBuffer();
+    for (int i = 0; i < text.length; i++) {
+      buffer.write(text[i]);
+      final index = i + 1;
+      if (index == 3 || index == 6) {
+        buffer.write('.');
+      } else if (index == 9) {
+        buffer.write('-');
+      }
+    }
+
+    final formattedText = buffer.toString();
+    return TextEditingValue(
+      text: formattedText,
+      selection: TextSelection.collapsed(offset: formattedText.length),
+    );
+  }
+}
+
+class CepInputFormatter extends TextInputFormatter {
+  @override
+  TextEditingValue formatEditUpdate(
+    TextEditingValue oldValue,
+    TextEditingValue newValue,
+  ) {
+    final text = newValue.text;
+    if (text.isEmpty) return newValue;
+
+    final buffer = StringBuffer();
+    for (int i = 0; i < text.length; i++) {
+      buffer.write(text[i]);
+      final index = i + 1;
+      if (index == 5) {
+        buffer.write('-');
+      }
+    }
+
+    final formattedText = buffer.toString();
+    return TextEditingValue(
+      text: formattedText,
+      selection: TextSelection.collapsed(offset: formattedText.length),
+    );
+  }
+}
